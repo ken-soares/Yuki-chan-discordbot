2c62067e8b604aa669ef153c5d008dd5fa62980c
ken-soares <kensoares@gmail.com>
2022-01-05 20:12:12 +0100

    added a hug command

--- cogs/relationships.py
+++ cogs/relationships.py
@@ -30,5 +30,29 @@ class Relationships(commands.Cog):
             embed.set_image(url=f"{file['kiss'][random.randint(0, maxkiss)]}")
             await ctx.send(embed=embed)
 
+    
+    @commands.command()
+    async def hug(self, ctx,*, user):
+        if type(user) == discord.User:
+            embed = discord.Embed(
+                    description = f"{ctx.message.author} hugged {user.mention}",
+                    colour = discord.Colour.purple()
+                    )
+
+            file = pandas.read_csv("cogs/ressources/relationships.csv")
+            maxhug = len(file['hug']) - 1
+            embed.set_image(url=f"{file['hug'][random.randint(0, maxhug)]}")
+            await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(
+                    description = f"{ctx.message.author} hugged {user}",
+                    colour = discord.Colour.purple()
+                    )
+
+            file = pandas.read_csv("cogs/ressources/relationships.csv")
+            maxkiss = len(file['hug']) - 1
+            embed.set_image(url=f"{file['hugged'][random.randint(0, maxhug)]}")
+            await ctx.send(embed=embed)
+
 def setup(client):
     client.add_cog(Relationships(client))
