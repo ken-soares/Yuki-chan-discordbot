302dc2e17f7575512e7d944f33b1a3b9ffbfe756
ken-soares <kensoares@gmail.com>
2022-01-05 19:44:58 +0100

    made a real setup script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,19 @@
-pip install discord
-pip install pandas
+#!/bin/bash
+
+dep=('discord' 'pandas')
+
+echo -n "Do you want to install required dependencies for Yuki-chan? (y/n):"
+read response
+
+if [ ${response,,} == 'y' ];
+then
+  for el in ${dep[@]}
+  do
+    pip install $el
+  done
+  clear
+  echo "Successfully installed all required dependencies ^-^"
+else
+  clear
+  echo "No dependencies will be installed."
+fi
